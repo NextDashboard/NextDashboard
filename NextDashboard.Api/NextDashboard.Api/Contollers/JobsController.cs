8fca4bbd97fbdafe0bbacdd2bb3177121e652fdb
danblack101 <danblack101@gmail.com>
2014-07-11 14:44:28 +1200

    Code skeleton

---
+++ NextDashboard.Api/NextDashboard.Api/Contollers/JobsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NextDashboard.Api.Contollers
+{
+    public class JobsController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ NextDashboard.Api/NextDashboard.Api/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace NextDashboard.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine("Dashboard API started");
+                Console.ReadLine(); 
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
