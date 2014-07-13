c439ba84cbc9f7fde2293b1edfe93b8849bf6533
danblack101 <danblack101@gmail.com>
2014-07-14 08:58:30 +1200

    Add logging

--- NextDashboard.Api/NextDashboard.Api/Program.cs
+++ NextDashboard.Api/NextDashboard.Api/Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Net.Http;
 using Microsoft.Owin.Hosting;
+using NLog;
 
 namespace NextDashboard.Api
 {
     class Program
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            string baseAddress = "http://localhost:9000/";
+            var baseAddress = "http://localhost:9000/";
 
+            _logger.Info("Starting API service");
             // Start OWIN host 
             using (WebApp.Start<Startup>(url: baseAddress))
             {
-                Console.WriteLine("Dashboard API started");
+                _logger.Info("Dashboard API started");
+                PingApiForSampleData(baseAddress);
+
+                _logger.Info("Press Any Key to stop API service");
                 Console.ReadLine(); 
             }
 
-            Console.ReadLine(); 
+        }
+
+        private static void PingApiForSampleData(string baseAddress)
+        {
+            var client = new HttpClient();
+            _logger.Trace("Retrieving sample Jobs");
+            var response = client.GetAsync(baseAddress + "api/Jobs").Result;
+            _logger.Trace(response);
+            _logger.Trace(response.Content.ReadAsStringAsync().Result);
         }
     }
 }
