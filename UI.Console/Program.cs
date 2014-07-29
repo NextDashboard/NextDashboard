9736e2cd0f3cafee7671cfced64381e4a7e9da02
johannesprinz <johannesprinz@gmail.com>
2014-07-29 08:22:26 +0000

    added new project structure, still to move old logic into new structure

---
+++ UI.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using NextDashboard.UI.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextDashboard.UI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                System.Console.WriteLine("Press [enter] to quit...");
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
