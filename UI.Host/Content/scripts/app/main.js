08f02492bbd7937791a471c764a1bd931e26abce
danblack101 <danblack101@gmail.com>
2014-08-07 10:32:50 +1200

    Add: Fake UI from angular

---
+++ UI.Host/Content/scripts/app/main.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var app = angular.module('nextDashboard', []);
+
+    var jobsFake = [
+        { name: 'Test CI Build', status: "Passed" },
+        { name: 'Integration Tests', status: "Failing" },
+        { name: 'System Tests', status: "Passing" },
+        { name: 'QA TestAutomation', status: "Passing" },
+        { name: 'Web App Deployment', status: "Failing" },
+        { name: 'TP Sprint 3', status: "3 of 7 Stories Completed" },
+        { name: 'Code Reviews', status: "25 Completed this week, 9 Pending" },
+    ];
+
+    app.controller('GroupController', function ($scope) {
+        this.jobs = jobsFake;
+    });
+})();
