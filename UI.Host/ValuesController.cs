2b794bbd7476e66ec8cdafb9c26f1994f548ee86
danblack101 <danblack101@gmail.com>
2014-08-07 09:00:36 +1200

    Refactor solution structure

---
+++ UI.Host/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace UI.Host
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
