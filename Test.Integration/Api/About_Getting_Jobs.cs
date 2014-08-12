e9b107ad9987b8481246d89214f4270612d424ac
danblack101 <danblack101@gmail.com>
2014-08-13 09:22:22 +1200

    Add: in memory owin api integration test

---
+++ Test.Integration/Api/About_Getting_Jobs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using FluentAssertions;
+using Microsoft.Owin.Testing;
+using NUnit.Framework;
+using NextDashboard.Application.DataContracts;
+using UI.Host;
+
+namespace NextDashboard.Test.Integration.Api
+{
+    [TestFixture]
+    public class About_Getting_Jobs
+    {
+        private TestServer _inMemoryServer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _inMemoryServer = TestServer.Create<Startup>();
+        }
+        
+        [Test]
+        public void I_Should_Be_Able_To_Get_A_List_Of_Jobs()
+        {
+            var response = _inMemoryServer.HttpClient.GetAsync("/api/job").Result;
+            var result = response.Content.ReadAsAsync<IEnumerable<Job>>().Result;
+
+            result.Count().Should().BeGreaterThan(0);
+        }
+
+
+
+
+    }
+}
