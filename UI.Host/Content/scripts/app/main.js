c65b03e3722ddba59969225bc8477ce1c78129a0
Dan Black <dblack@boardbooks.com>
2014-09-26 13:41:37 +1200

    Add: First signlar refresh hub
    TPB95253 Self Service Password Reset is throwing an unhandled exception
    TPU90016 Audit PW Changes in the Web Client
    TPF6546 Audit - Audit Actions

--- UI.Host/Content/scripts/app/main.js
+++ UI.Host/Content/scripts/app/main.js
@@ -12,5 +12,17 @@
             console.log(response.data);
             $scope.jobs = response.data;
         });
+
+        $scope.refresh = function () {
+            console.log("Todo Wire up refresh here");
+            //Todo Wire up refresh here
+            var chat = $.connection.refreshHub;
+            chat.server.refreshJob('');
+            $http.get("/api/refresh/1").then(function (response) {
+                console.log('refreshed!');
+                console.log(response.data);
+            });
+
+        }
     });
 })();
---
+++ UI.Host/Hubs/RefreshHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace UI.Host.Hubs
+{
+    public class RefreshHub : Hub
+    {
+
+        public void RefreshJob(string jobId)
+        {
+            
+        }
+    }
+}
