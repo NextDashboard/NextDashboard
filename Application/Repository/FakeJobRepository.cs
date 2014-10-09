aaa2e561a47db3f69cb3d161d296b43d9629544f
Dan Black <dblack@boardbooks.com>
2014-10-10 09:12:02 +1300

    UI formatting, adding bootstrap flat

--- Application/Repository/FakeJobRepository.cs
+++ Application/Repository/FakeJobRepository.cs
@@ -12,19 +12,17 @@ namespace NextDashboard.Application.Repository
         {
             return new List<Job>
             {
-                new JenkinsJob("1","Jenkins Auditing CI", "Passing", JenkinsBaseUrl, "BB - CI/job/Auditing CI","dblack","5dd12d253bb6ff1376f3b5a5a9333712"),
-                new JenkinsJob("2","Jenkins Integration Tests", "Passing", JenkinsBaseUrl, "Tests/job/Kanga/job/Kanga - Integration Tests","dblack","5dd12d253bb6ff1376f3b5a5a9333712"),
-                new JenkinsJob("3","Test Job 3", "Failing", JenkinsBaseUrl, "BB - CI/job/Auditing CI","dblack","5dd12d253bb6ff1376f3b5a5a9333712"),
-                new JenkinsJob("4","QA TestAutomation", "Failing", JenkinsBaseUrl, "BB - CI/job/Auditing CI","dblack","5dd12d253bb6ff1376f3b5a5a9333712"),
-                new JenkinsJob("5","TP Sprint 3", "3 of 7 Stories Completed", JenkinsBaseUrl, "BB - CI/job/Auditing CI","dblack","5dd12d253bb6ff1376f3b5a5a9333712"),
-                new JenkinsJob("6","Code Reviews", "25 Completed this week, 9 Pending", JenkinsBaseUrl, "BB - CI/job/Auditing CI","dblack","5dd12d253bb6ff1376f3b5a5a9333712"),
-                new SampleJob("7","Test Job 4","Passing")
+                new JenkinsJob("1", "Jenkins Auditing CI", "Passing", JenkinsBaseUrl, "BB - CI/job/Auditing CI","dblack", "5dd12d253bb6ff1376f3b5a5a9333712"),
+                new JenkinsJob("2", "Jenkins Integration Tests", "Passing", JenkinsBaseUrl,"Tests/job/Kanga/job/Kanga - Integration Tests", "dblack", "5dd12d253bb6ff1376f3b5a5a9333712"),
+                new SampleJob("3", "Code Reviews", "25 Completed this week, 9 Pending"),
+                new SampleJob("4", "TP Sprint 3", "3 of 7 Stories Completed"),
+                new SampleJob("5", "Test Job 4", "Passing")
             };
         }
 
         public Job Select(int jobId)
         {
-            return new JenkinsJob("1","Test Job 1", "Passing", JenkinsBaseUrl, "BB - CI/job/Auditing CI", "dblack", "5dd12d253bb6ff1376f3b5a5a9333712");
+            return new JenkinsJob("1", "Test Job 1", "Passing", JenkinsBaseUrl, "BB - CI/job/Auditing CI", "dblack","5dd12d253bb6ff1376f3b5a5a9333712");
         }
     }
 }
